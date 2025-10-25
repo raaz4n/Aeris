c0734b28a62733364c9c0b7e75c43e44e55d2684
raaz4n <raazandoski@gmail.com>
2025-10-25 12:31:17 -0500

    test

--- Aeris.cpp
+++ Aeris.cpp
@@ -1,26 +1,55 @@
 #include <iostream>
 #include <cstdlib>
 #include <cpr/cpr.h>
+#include <nlohmann/json.hpp>
+#include <fstream>
+#include <algorithm>
 using namespace std;
+using json = nlohmann::json;
 
 int main(){
-    string API_KEY = getenv("API_KEY");
+    //string API_KEY = getenv("API_KEY");
+    ifstream file("test.txt");
+    string API_KEY;
+    getline(file, API_KEY);
     string link; 
     string city;
     string state;
     string country;
-    cout << "Enter a city: ";
-    cin >> city;
-    cout << "Enter a state (only for US, type -1 to disregard): ";
-    cin >> state;
-    cout << "Enter a country: ";
-    cin >> country;
-    if (state == "-1"){
-        link = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "," + country + "&limit=1&appid=" + API_KEY;
+    string unit;
+
+
+    cout << "Enter a city: ";                                               // Format the city to be link friendly, as cpr seems to not automatically handle spaces
+    getline(cin, city);
+    for (int i = 0; i < city.length(); i++){
+        if (city[i] == ' '){
+            city[i] = '+';
+        }
+    }
+
+    cout << "Enter a state code (only for US, type -1 to disregard): ";     // Enter the state code (NY, CA, etc.)
+    getline(cin, state);
+
+    cout << "Enter a country code: ";
+    getline(cin, country);                                                  // Enter the country code (CN, QA, MX, etc.)
+
+    cout << "Metric or imperial?: ";                                        // User must input metric or imperial
+    getline(cin, unit);
+    transform(unit.begin(), unit.end(), unit.begin(), ::tolower);
+
+    if (unit == "metric" || unit == "imperial"){
+        if (state == "-1"){
+            link = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + country + "&units=" + unit + "&lang=en&appid=" + API_KEY;
+        }
+        else{
+            link = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + state + "," + country + "&units=" + unit + "&lang=en&appid=" + API_KEY;
+        }
     }
     else{
-        link = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "," + state + "," + country + "&limit=1&appid=" + API_KEY;
+        cout << "Invalid unit!" << endl;
     }
+
     cpr::Response r = cpr::Get(cpr::Url{link});
-    cout << r.text;
+    json response = json::parse(r.text);
+    cout << setw(4) << "Temperature: " << response["main"]["temp"]  << endl << setw(4) << "Weather: " << response["weather"][0]["main"] << endl;
 }
