0125948936128cf7673ad789f19eae9b989c9291
raaz4n <raazandoski@gmail.com>
2025-11-01 12:53:10 -0500

    Added search function implementation

--- static/Aeris.js
+++ static/Aeris.js
@@ -10,9 +10,18 @@ input.addEventListener('input', function(thing) {
             .then(response => response.json())
             .then(data => { data.forEach(location => {
                 const item = document.createElement('div');
-                item.textContent = `${location.name}, ${location.country}`;
+                if (location.state) {
+                    item.textContent = `${location.name}, ${location.state}, ${location.country}`;
+                } else {
+                    item.textContent = `${location.name}, ${location.country}`;
+                }
                 locationList.appendChild(item);
+                item.addEventListener('click', function() {
+                    document.getElementById('city').value = location.name;
+                    document.getElementById('state').value = location.state || '';
+                    document.getElementById('country').value = location.country;
+                });
             });
         });
-    }, 200);
+    }, 500);
 });
